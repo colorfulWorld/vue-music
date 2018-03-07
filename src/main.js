277b3e125860c880a580390efc617af611c13680
yu <740117123@qq.com>
2018-03-07 13:06:22 +0800

    feat(header):构建header组件

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App'
+import 'babel-polyfill';
+import Vue from 'vue';
+import App from './App';
+import fastclick from 'fastclick';
 // import router from './router'
+fastclick.attach(document.body);
 
-import 'common/less/index.less'
+import 'common/less/index.less';
 
 Vue({
   el: '#app',
   render: h => h(App)
-})
+});
